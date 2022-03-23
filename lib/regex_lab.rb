4da73c0fcf168f45dfb11750f47477a597593473
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-03-23 08:20:38 -0700

    commit tree 197481b214b0fd5b983f763e4d3ae64e23ff5f9b

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,23 @@
 def starts_with_a_vowel?(word)
-
+    # word.match(/\A[AaEeYyUuOoIiWw]/) ? true : false
+    word.match?(/\A[aeyuoiw]/i)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+   text.scan(/\bun\w*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    # text.scan(/^[A-Z]/).empty? || text.scan(/\W$/).empty? ? false : true
+    text.match?(/\A[A-Z](\w|\W)+[!?\.]\z/)
+    text.match?(/\A[A-Z].*[!?\.]\z/)
 end
 
 def valid_phone_number?(phone)
-
+   
+    phone.scan(/\d/).length>=10
 end
